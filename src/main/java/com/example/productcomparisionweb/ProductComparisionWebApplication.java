07614d77ecfc6615fdacc8da5b7d9e94a773f13d
Tsuki0512 <647752235@qq.com>
2024-10-27 21:51:21 +0800

    init

---
+++ src/main/java/com/example/productcomparisionweb/ProductComparisionWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.productcomparisionweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductComparisionWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductComparisionWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/productcomparisionweb/ProductComparisionWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productcomparisionweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductComparisionWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
